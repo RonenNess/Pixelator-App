44fdd01968bd447c1543b41f1b364a12d6844f64
Ronen <RonenNess@gmail.com>
2018-02-19 01:42:58 +0200

    ..

--- js/shared.js
+++ js/shared.js
@@ -2,10 +2,12 @@
 {
 
     // get current page
-    var curr = location.href.split('/').reverse()[0].split('#')[0].split('?')[0]
+    var curr = location.pathname.substr(1);
 
     // highlight navbar
-    $("nav").find(".active").removeClass('active');
-    $("nav").find('a[href$="' + curr + '"]').parent().addClass('active');
-
+    if (curr.length > 0)
+    {
+        $("nav").find(".active").removeClass('active');
+        $("nav").find('a[href$="' + curr + '"]').parent().addClass('active');
+    }
 })();
